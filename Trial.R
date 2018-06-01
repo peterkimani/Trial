3d8a72ea5c081845ad763765f90da6dc4de19f33
Peter K <peter.kimani@aiesec.net>
2018-06-01 03:24:04 +0300

    updated on the sqdlf function

--- Trial.R
+++ Trial.R
@@ -14,15 +14,35 @@ births.recode<-read.dta13("C:\\Users\\sam-pc\\Documents\\pitz\\Bsc Biostatistics
 #b7= age at death (months)
 #v001 = cluster number 
 #v005= sample weight 
-my.data<-births.recode[,c("b2","b5","b11","b12","m15","m19","m17","m18","b7","v001","v005")]
+my_data<-births.recode[,c("b2","b5","b6","b11","b12","m15","m19","m17","m18","b7","v001","v005")]
+#limiting the sample to only children born between 2010 and 2014
+my_data<-subset(my_data,b2 %in% 2010:2014)
+#creating weight variable 
+my_data$weight<-my_data$v005/100000
+#creating an infant mortality variable 
+my_data$im_variable<-as.numeric(my_data$b5 %in% "no")
+#creaing variable for ceaserian section 
+my_data$cs<-as.numeric(my_data$m17 %in% "yes")
+#selecting rows that have only preceeding and succeeding birth intervals 
+cases<-my_data[complete.cases(my_data[,c(5:4)]),]
+
+#calculating four statistics 
+library(sqldf)
+deaths <- sqldf( "select v001 as dhsclust , count(*) as children , sum( im_variable ) as total_deaths , Avg( b11 ) as preceeding_birthinterval, Avg(b12) as succeeding_birthinterval, sum(cs) as ceaserian_births, sum( weight ) as sum_weight, sum( im_variable * weight ) as weighted_deaths from cases group by v001" )
+
 #manipulating data \
 #changing b11 and b12 from numeric to factor 
 
-library(dplyr)
+#library(dplyr)
 #extracting data for only children who are dead 
-my.data<-my.data[,my.data$b6=="no"]
-my.data %>% group_by(my.data$v001) %>% 
-  summarise(child_is_alive =sum(b5=="yes"),
-            child_is_dead=sum(b5=="no"),
-            preceeding_birth_interval=mean(b11,na.rm=TRUE))
+#f.data<-my_data %>% group_by(my_data$v001) %>% 
+  #summarise(child_is_alive =sum(b5=="yes"),
+            #child_is_dead=sum(b5=="no"),
+            #preceeding_birth_interval=mean(b11,na.rm=TRUE),
+            #succeeding_birth_interval=mean(b12,na.rm=TRUE),
+            #birth_weight=mean(as.numeric(m19),na.rm=TRUE))
+
+#modelling the counts 
+library(FWDselect)
+
 
